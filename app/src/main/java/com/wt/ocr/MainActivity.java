5bba1cefbba670716a3d740766dd53ec773157ee
wangT <e067228080@163.com>
2018-03-27 13:57:05 +0800

    增加摄像头权限

--- app/src/main/java/com/wt/ocr/MainActivity.java
+++ app/src/main/java/com/wt/ocr/MainActivity.java
@@ -1,9 +1,15 @@
 package com.wt.ocr;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.analytics.HitBuilders;
 
@@ -13,6 +19,9 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final int PERMISSIONS_REQUEST_CAMERA = 454;
+    static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
 
     private ImageView imageView;
 
@@ -36,9 +45,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_camera:
-                Intent intent = new Intent(this, TakePhoteActivity.class);
-                startActivity(intent);
+                checkSelfPermission();
 
+                //google分析
                 getTracker().send(new HitBuilders.EventBuilder()
                         .setCategory("Action")
                         .setAction("拍照")
@@ -86,8 +95,38 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
 
+    //google分析
     private void sendScreenImageName() {
         getTracker().setScreenName("Activity-" + "首页");
         getTracker().send(new HitBuilders.ScreenViewBuilder().build());
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, TakePhoteActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "请开启摄像头权限", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * 检查权限
+     */
+    void checkSelfPermission() {
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{PERMISSION_CAMERA}, PERMISSIONS_REQUEST_CAMERA);
+        } else {
+            Intent intent = new Intent(this, TakePhoteActivity.class);
+            startActivity(intent);
+        }
     }
 }
